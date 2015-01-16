af136533defbcf27be5b839f21dfc22d1e393197
lawrence0192 <lawrence0192@yahoo.com>
2015-01-15 20:54:38 -0500

    Update helper.js

--- js/helper.js
+++ js/helper.js
@@ -58,8 +58,6 @@ var HTMLonlineURL = '<br><a href="#">%data%</a>';
 var internationalizeButton = '<button>Internationalize</button>';
 var googleMap = '<div id="map"></div>';
 
-var formattedName = HTMLheaderName.replace("%data%", name);
-$("#header").append(formattedName);
 /*
 The International Name challenge in Lesson 2 where you'll create a function that will need this helper code to run. Don't delete! It hooks up your code to the button you'll be appending.
 */
