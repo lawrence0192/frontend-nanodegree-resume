9773aab6a1a295f15a0259cfa289277c811c4fd8
lawrence0192 <lawrence0192@yahoo.com>
2015-01-15 21:29:13 -0500

    Update resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,4 +1,5 @@
 var formattedName = HTMLheaderName.replace("%data%", "Lawrence Johnson");
+var role = "Web Developer";
 var formattedRole = HTMLheaderRole.replace("%data%", "Web Developer");
 $("#header").prepend(formattedRole);
 $("#header").prepend(formattedName);
