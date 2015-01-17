6db50d31cb4bff002ba3a80c91e0bd4a69b2eb80
lawrence0192 <lawrence0192@yahoo.com>
2015-01-16 21:57:21 -0500

    Update resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -3,3 +3,13 @@ var role = "Web Developer";
 var formattedRole = HTMLheaderRole.replace("%data%", "Web Developer");
 $("#header").prepend(formattedRole);
 $("#header").prepend(formattedName);
+var skills = ["awesomeness", "programming", "teaching", "JS"];
+//$("#main").append(skills);
+//$("#main").append(skills[0]);
+//$("#main").append(skills.length);
+var bio = {
+  "name" : "Lawrence",
+  "age" : 29,
+  "skills" : skills
+};
+$("#main").append(bio.name);
