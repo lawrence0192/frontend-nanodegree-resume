5c2d98cfd89ecfbb71e7535546d4ccedda676b63
lawrence0192 <lawrence0192@yahoo.com>
2015-01-19 18:50:13 -0500

    Update resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,13 +1,4 @@
-var formattedName = HTMLheaderName.replace("%data%", "Lawrence Johnson");
-//var role = "Web Developer";
-var formattedRole = HTMLheaderRole.replace("%data%", "Web Developer");
-$("#header").prepend(formattedRole);
-$("#header").prepend(formattedName);
 
-//var skills = ["awesomeness", "programming", "teaching", "JS"];
-//$("#main").append(skills);
-//$("#main").append(skills[0]);
-//$("#main").append(skills.length);
 var bio = {
 "name": "Lawrence Johnson",
 "role": "Web Developer",
@@ -21,73 +12,10 @@ var bio = {
 "welcomeMessage": "lorem ipsum dolor sit amet etc etc etc.",
 "skills": [
   "awesomeness", "delivering things", "cryogenic sleep", "saving the universe"
-  ],
-  "bioPic": "images/fry.jpg"
-  
-}
-  //$("#header").append(bio.name);
-  //$("#header").append(bio.role);
-  var education = {
-    "schools": [
-      {
-        "name": "Virginia Commonwealth University",
-        "city": "Richmond, VA",
-        "degree": "associates",
-        "major": ["Computer Engineering"],
-        "dates": 2004,
-        "url": "http://www.vcu.edu"
-      },
-      {
-        "name": "Strayer University",
-        "city": "Richmond, VA",
-        "degree": "bachelors",
-        "major": ["computer networking"],
-        "dates": 2013,
-        "url": "http://www.strayer.edu"
-      }
-    ],
-    "online courses": [
-      {
-        "title": "JavaScript Crash Course",
-        "school": "Udacity",
-        "dates": 2014,
-        "url": "http://www.udacity.com/course/ud804"
-      }
-    ]
-  }
-  
-  var work = {
-  "jobs": [
-  {
-    "employer": "Capital One",
-    "title": "Fraud Account Supervisor",
-    "dates": "2010 - Present",
-    "description": "I answer questions for employees who work in the fraud department and take fraud phone calls.  When they arent sure what do to, I offer assistance to help them out"
-  },
-  {
-    "employer": "Besy Buy",
-    "title": "Media Supervisor",
-    "dates": "2005-2010",
-    "description": "I sold music, movies, and video games to customers"
-  },
-  {
-    "employer": "McDonald's",
-    "title": "Manager",
-    "dates": "2002-2004",
-    "description": "I cooked food, took customers' orders, and managed a staff of about 75 people."
-  }
   ]
-  }
+};
   
-  var projects = {
-    "projects": [
-      {
-    "title": "Sample Project 1",
-    "dates": 2014,
-    "description": "who moved my cheese cheesy feet cauliflower cheese."
-      ]
-  }
-  }
+
     
     
     
