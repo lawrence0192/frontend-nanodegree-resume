bdd8ee7f9f5f29a3027687a4974b05402e9284e2
lawrence0192 <lawrence0192@yahoo.com>
2015-01-19 21:46:19 -0500

    Update resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -3,13 +3,13 @@ var bio = {
 "name": "Lawrence Johnson",
 "role": "Web Developer",
 "contacts": {
-    "mobile": "804-445-3842",
+    "mobile": "804-445-xxxx",
     "email": "lawrence0192@yahoo.com",
     "github": "lawrence0192",
     "twitter": "@lawrence",
     "location": "Richmond"
   },
-"bioPic": "images/fry.jpg"  
+"picture": "images/fry.jpg"  
 "welcomeMessage": "lorem ipsum dolor sit amet etc etc etc.",
 "skills": [
     "awesomeness", "delivering things", "cryogenic sleep", "saving the universe"
